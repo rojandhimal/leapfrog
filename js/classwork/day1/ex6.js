1da81588f781b0cae7e083cf5bc3b41b2b1ad4bf
Rojan116 <rojandhimal1@gmail.com>
2019-11-26 10:59:52 +0545

    manage js dir

---
+++ js/classwork/day1/ex6.js
@@ -0,0 +1,49 @@
+var input = {
+    '1': {
+      id: 1,
+      name: 'John',
+      children: [
+        { id: 2, name: 'Sally' },
+        { id: 3, name: 'Mark', children: [{ id: 4, name: 'Harry' }] }
+      ]
+    },
+    '5': {
+      id: 5,
+      name: 'Mike',
+      children: [{ id: 6, name: 'Peter' }]
+    }
+  };
+var tempObj = new Object();
+var output = new Object();
+
+function objCopy(srcObj) {
+    var targetObj = new Object();
+    for(key in srcObj){
+        targetObj[key] = srcObj[key];
+    }
+    return targetObj;
+}
+
+function normalize(obj){  
+    for(key in obj){
+        var element = obj[key];
+        if(element.hasOwnProperty("children")){
+            tempObj.id = element["id"];
+            tempObj.name = element["name"];
+            tempObj.children = [];
+            for(childElement in element["children"]){
+              tempObj.children.push(childElement["id"]);
+            }
+            Object.defineProperty(output, element["id"], {value: objCopy(tempObj), configurable: true});
+            delete tempObj["children"];
+            normalize(element["children"]);
+        }
+        else{
+            tempObj.id = element["id"];
+            tempObj.name = element["name"];
+            Object.defineProperty(output, element["id"], {value: objCopy(tempObj), configurable: true});
+        }
+    }
+    return output;
+}
+console.log("OUTPUT = \n",normalize(input));
